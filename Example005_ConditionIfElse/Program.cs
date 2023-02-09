96629fb06daa8f937445a98572c3d2d39f327d16
maksim bykov <mak-42@list.ru>
2023-02-09 21:50:53 +0300

    add Example005_Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "максим") 
+{
+    Console.WriteLine("Ура Это же Макс!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
